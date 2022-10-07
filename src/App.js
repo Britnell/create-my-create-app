f285b6a2fd8fc76fab27593c20791ed913a287d8
Tommy <t.a.britnell@gmail.com>
2022-10-07 09:36:26 +0200

    lazy load comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import Counter from "./Counter";
+// import Counter from "./Counter";
+import React from "react";
+const Counter = React.lazy(() => import("./Counter"));
 
 const App = () => {
   return (
     <div>
       <h1>App</h1>
-      <Counter init={7} />
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <Counter init={7} />
+      </React.Suspense>
     </div>
   );
 };
